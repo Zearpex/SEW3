25ae9fdb7bfd88f9045232139b05fb729bc76924
Zearpex <felix.kurz@sdsystems.at>
2017-09-25 11:40:31 +0200

    hey

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@
  * Created by 5063 on 25.09.2017.
  */
 public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello from the htl");
+        return "haha";
+    }
 }
